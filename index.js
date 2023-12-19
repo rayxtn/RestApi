4dcb8c2ebbb5352e164c55ad7aa4cb86a6238aa0
RAED HOUIMLI <houimliraed@outlook.fr>
2023-12-19 14:45:55 +0100

    api query

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-import { Express } from "express";
+import Express from "express";
 const app = Express();
+
+
+app.get("/", (req,res)=>{
+const user = req.query.user;
+res.send(user);
+})
+
+app.listen(5000, ()=>{
+    console.log('server started on port 5000');
+})
