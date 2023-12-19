3bff85a05e30d581a8dbdb0145b50eff35ca8dd5
RAED HOUIMLI <houimliraed@outlook.fr>
2023-12-19 17:19:42 +0100

    middleware and create user

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import Express from "express";
 const app = Express();
 
-
+app.use(Express.json());
 app.get("/", (req,res)=>{
 const user = req.query.user;
 res.send(user);
-})
+});
+
+const users =[];
+app.post("/create_user",(req,res) => {
+    const {user}= req.body;
+    users.push({username:user.username,password:user.password});
+    res.json({loggedIn:true,status:"success"});
+}
+
+)
+
+
 
 app.listen(5000, ()=>{
     console.log('server started on port 5000');
